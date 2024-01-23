36c57ebc552d47daeddc8d8b64dfb21fdfe36906
HersheyTriesToCode <h.rupani@uq.edu.au>
2024-01-24 08:41:28 +1000

    first commit of the docker part of the umi pipeline

---
+++ nanopore_umi_error_correction/ngspeciesid/run_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --rm --name umi-pipeline-ngspeciesid -v ~/src:/usr/src -v ~/data:/usr/data ngspeciesid
---
+++ nanopore_umi_error_correction/umi/run_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --rm --name umi-pipeline-extract -v ~/src:/usr/src -v ~/data:/usr/data umi
